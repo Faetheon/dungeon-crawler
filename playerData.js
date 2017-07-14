b6df7292176dde223d32dd1f89a1efd18f38b9bd
Jesse Hill <jsnahill@gmail.com>
2017-07-13 23:02:43 -0500

    Create structure for player data for brother to build out

---
+++ playerData.js
@@ -0,0 +1,107 @@
+
+class Player = {
+  constructor(name, role, image, location) {
+    this.info = {
+      name,
+      role,
+      image,
+      location
+    };
+    this.stats = roles[role];
+    this.bag = {};
+  }
+
+};
+
+const roles = {
+  tank: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  assasin: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  ranger: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  warrior: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  healer: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  mage: {
+    // health
+    // attack
+    // defense
+    // attackType
+  }
+};
+
+class Enemy = {
+  constructor(type, location, image) {
+    this.info = {
+      type,
+      location,
+      image
+    };
+
+    this.stats = enemyTypes[type];
+  }
+};
+
+const enemyTypes = {
+  // make these whatever you want
+  tank: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  assasin: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  ranger: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  warrior: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  healer: {
+    // health
+    // attack
+    // defense
+    // attackType
+  },
+  mage: {
+    // health
+    // attack
+    // defense
+    // attackType
+  }
+};
+
+const player1 = new Player();
